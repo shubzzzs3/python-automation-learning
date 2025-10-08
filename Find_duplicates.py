2c6f70525232d8d9d0b1069f31dddfe1a11e5b9a
Shubham <subushirke@gmail.com>
2025-10-08 12:04:33 +0530

    Find Duplicates in list

---
+++ Find_duplicates.py
@@ -0,0 +1,14 @@
+#Find duplicate elements in a list.
+num = input("Enter the num : ").split() #split uses because takinh multi digit input
+unique=[]
+dup=[]
+for i in num:
+    if i not in unique:
+        unique.append(i)
+    elif i not in dup:
+        dup.append(i)
+    
+
+print (num)
+print(f"duplicates are {dup}")
+print(f"uniques are {unique}")
